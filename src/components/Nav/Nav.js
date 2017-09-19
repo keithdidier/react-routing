ef1eff376866fcaacfaa0ee062b375ef8dc0e49b
keithdidier <keith.b.didier@gmail.com>
2017-09-19 12:29:06 -0600

    Done

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
 
 import './Nav.css';
@@ -9,12 +10,14 @@ function Nav( { items } ) {
   return (
     <div id="Nav__container">
       <div id="Nav__linksContainer">
-        <span className="Nav__label">Store</span>
+        <Link to='/' className="navLink"><span className="Nav__label">Store</span></Link>
 
-        <div id="Nav__checkoutContainer">
-          <div id="Nav__itemsInCart"> { items } </div>
-          <ShoppingCart id="Nav__shoppingCart" />
-        </div>
+        <Link to='/checkout' className="navLink">
+          <div id="Nav__checkoutContainer">
+            <div id="Nav__itemsInCart"> { items } </div>
+            <ShoppingCart id="Nav__shoppingCart" />
+          </div>
+        </Link>
       </div>
     </div>
   )
